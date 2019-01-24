77fcdfcfce1da56115ae655af16036820f5f8b38
Nathan Klapstein <nklapste@ualberta.ca>
2019-01-24 14:37:19 -0700

    adding python package boilerplate

---
+++ client/__main__.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""argparse and main entrypoint script for the deadchat client"""
+
+import argparse
+import logging
+import os
+import sys
+from logging.handlers import TimedRotatingFileHandler
+
+LOG_LEVEL_STRINGS = ["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG"]
+
+
+def log_level(log_level_string: str):
+    """Argparse type function for determining the specified logging level"""
+    if log_level_string not in LOG_LEVEL_STRINGS:
+        raise argparse.ArgumentTypeError(
+            "invalid choice: {} (choose from {})".format(
+                log_level_string,
+                LOG_LEVEL_STRINGS
+            )
+        )
+    return getattr(logging, log_level_string, logging.INFO)
+
+
+def add_log_parser(parser):
+    """Add logging options to the argument parser"""
+    group = parser.add_argument_group(title="Logging")
+    group.add_argument("--log-level", dest="log_level", default="INFO",
+                       type=log_level, help="Set the logging output level")
+    group.add_argument("--log-dir", dest="log_dir",
+                       help="Enable TimeRotatingLogging at the directory "
+                            "specified")
+    group.add_argument("-v", "--verbose", action="store_true",
+                       help="Enable verbose logging")
+
+
+def init_logging(args, log_file_path):
+    """Intake a argparse.parse_args() object and setup python logging"""
+    # configure logging
+    handlers_ = []
+    log_format = logging.Formatter(fmt="[%(asctime)s] [%(levelname)s] - %(message)s")
+    if args.log_dir:
+        os.makedirs(args.log_dir, exist_ok=True)
+        file_handler = TimedRotatingFileHandler(
+            os.path.join(args.log_dir, log_file_path),
+            when="d", interval=1, backupCount=7, encoding="UTF-8",
+        )
+        file_handler.setFormatter(log_format)
+        file_handler.setLevel(args.log_level)
+        handlers_.append(file_handler)
+    if args.verbose:
+        stream_handler = logging.StreamHandler(stream=sys.stderr)
+        stream_handler.setFormatter(log_format)
+        stream_handler.setLevel(args.log_level)
+        handlers_.append(stream_handler)
+
+    logging.basicConfig(
+        handlers=handlers_,
+        level=args.log_level
+    )
+
+
+def get_parser() -> argparse.ArgumentParser:
+    """Create and return the argparser for the deadchat client"""
+    parser = argparse.ArgumentParser(
+        description="Start the deadchat client",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    add_log_parser(parser)
+
+    return parser
+
+
+def main(argv=sys.argv[1:]) -> int:
+    """main entry point for the deadchat client"""
+    parser = get_parser()
+    args = parser.parse_args(argv)
+
+    init_logging(args, "deadchat_client.log")
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ setup.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""setup.py for the deadchat client"""
+
+import codecs
+import os
+import re
+import sys
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test
+
+
+def find_version(*file_paths):
+    with codecs.open(os.path.join(os.path.abspath(os.path.dirname(__file__)), *file_paths), "r") as fp:
+        version_file = fp.read()
+    m = re.search(r"^__version__ = \((\d+), ?(\d+), ?(\d+)\)", version_file, re.M)
+    if m:
+        return "{}.{}.{}".format(*m.groups())
+    raise RuntimeError("Unable to find a valid version")
+
+
+VERSION = find_version("client", "__init__.py")
+
+
+class Pylint(test):
+    user_options = [('pylint-args=', 'a', "Arguments to pass to pylint")]
+
+    def initialize_options(self):
+        test.initialize_options(self)
+        self.pylint_args = "client --persistent=y --rcfile=.pylintrc --output-format=colorized"
+
+    def run_tests(self):
+        import shlex
+        # import here, cause outside the eggs aren't loaded
+        from pylint.lint import Run
+        Run(shlex.split(self.pylint_args))
+
+
+class PyTest(test):
+    user_options = [("pytest-args=", "a", "Arguments to pass to pytest")]
+
+    def initialize_options(self):
+        test.initialize_options(self)
+        self.pytest_args = "-v --cov={}".format("client")
+
+    def run_tests(self):
+        import shlex
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(shlex.split(self.pytest_args))
+        sys.exit(errno)
+
+
+def readme():
+    with open("README.md", encoding="utf-8") as f:
+        return f.read()
+
+
+setup(
+    name="deadchat-client",
+    version=VERSION,
+    description="",
+    long_description=readme(),
+    author="Nathan Klapstein",
+    author_email="nklapste@ualberta.ca",
+    url="https://github.com/nklapste/deadchat",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+    packages=find_packages(exclude=["test"]),
+    include_package_data=True,
+    install_requires=[
+    ],
+    tests_require=[
+        "pytest>=4.1.0,<5.0.0",
+        "pytest-cov>=2.6.1,<3.0.0",
+        "pylint>=2.2.2,<3.0.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "client = client.__main__:main",
+        ],
+    },
+    cmdclass={"test": PyTest, "lint": Pylint},
+)
