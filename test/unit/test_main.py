9e184d53942ea794e35f0fa89a4a0f035bd7fbed
Nathan Klapstein <nklapste@ualberta.ca>
2019-01-31 08:51:19 -0700

    improving tests

--- test/unit/test_main.py
+++ test/unit/test_main.py
@@ -5,10 +5,13 @@
 
 import argparse
 import logging
+from unittest.mock import patch
 
+import nacl.secret
+import nacl.utils
 import pytest
 
-from dead_sfs.__main__ import get_parser, log_level
+from dead_sfs.__main__ import get_parser, log_level, main
 
 
 def test_get_parser():
@@ -34,3 +37,34 @@ def test_log_level_invalid():
     with pytest.raises(argparse.ArgumentTypeError):
         log_level("nonsuch")
 
+
+def test_main_missing_key_arg():
+    with pytest.raises(SystemExit):
+        main([])
+
+
+@pytest.fixture()
+def secret_key_file(tmpdir):
+    keyfile = tmpdir.mkdir("keys").join("deadSFS.key")
+    with open(str(keyfile), "wb") as f:
+        f.write(nacl.utils.random(nacl.secret.SecretBox.KEY_SIZE))
+    return keyfile
+
+
+@patch("dead_sfs.shell.DeadSFSShell.cmdloop")
+def test_main(mock_dead_sfs_shell_cmdloop, secret_key_file):
+    main([str(secret_key_file)])
+    mock_dead_sfs_shell_cmdloop.assert_called_once_with()
+
+
+@patch("dead_sfs.shell.DeadSFSShell.cmdloop")
+def test_main_verbose(mock_dead_sfs_shell_cmdloop, secret_key_file):
+    main([str(secret_key_file), "-v"])
+    mock_dead_sfs_shell_cmdloop.assert_called_once_with()
+
+
+@patch("dead_sfs.shell.DeadSFSShell.cmdloop")
+def test_main_log_dir(mock_dead_sfs_shell_cmdloop, tmpdir, secret_key_file):
+    logging_dir = str(tmpdir.mkdir("logs"))
+    main([str(secret_key_file), "--log-dir={}".format(logging_dir)])
+    mock_dead_sfs_shell_cmdloop.assert_called_once_with()
